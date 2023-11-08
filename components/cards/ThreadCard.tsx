77fb1677a5eab1065d50319e296a495beff12e3c
ezzelddin <ezzelddinabdallah@hotmail.com>
2023-11-09 00:37:03 +0200

    allow line breaks in bio & threads

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -72,7 +72,9 @@ async function ThreadCard({
 							</h4>
 						</Link>
 
-						<p className="mt-2 text-small-regular text-light-2">{content}</p>
+						<p className="mt-2 text-small-regular text-light-2 whitespace-pre-line">
+							{content}
+						</p>
 
 						<div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
 							<div className="flex gap-3.5">
